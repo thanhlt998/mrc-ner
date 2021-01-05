b8b1f5c2a71a73781eb9c924398f0344e5055834
Lê Tuấn Thành (VNCDLL-CTÐTKSAI) <thanhlt41@vingroup.net>
2021-01-05 09:33:48 +0700

    add adaptive loss

--- loss/loss_func_fastai.py
+++ loss/loss_func_fastai.py
@@ -77,3 +77,77 @@ class CustomLoss(nn.Module):
             + self.weight_end * end_loss
             + self.weight_span * match_loss
         )
+
+
+class CustomAdaptiveLoss(nn.Module):
+    def __init__(
+            self,
+            n_task=3,
+            span_loss_candidates='all',
+            loss_type='bce',
+            dice_smooth=1e-8,
+    ):
+        super(CustomAdaptiveLoss, self).__init__()
+        self.loss_type = loss_type
+        self.span_loss_candidates = span_loss_candidates
+
+        if self.loss_type == "bce":
+            self.bce_loss = BCEWithLogitsLoss(reduction="none")
+        else:
+            self.dice_loss = DiceLoss(with_logits=True, smooth=dice_smooth)
+
+        self.log_vars = nn.Parameter(torch.zeros(n_task))
+
+    def forward(self, input, *target):
+        start_logits, end_logits, span_logits = input
+        start_labels, end_labels, start_label_mask, end_label_mask, match_labels = target
+
+        batch_size, seq_len = start_logits.size()
+
+        start_float_label_mask = start_label_mask.view(-1).float()
+        end_float_label_mask = end_label_mask.view(-1).float()
+        match_label_row_mask = start_label_mask.bool().unsqueeze(-1).expand(-1, -1, seq_len)
+        match_label_col_mask = end_label_mask.bool().unsqueeze(-2).expand(-1, seq_len, -1)
+        match_label_mask = match_label_row_mask & match_label_col_mask
+        match_label_mask = torch.triu(match_label_mask, 0)  # start should be less equal to end
+
+        if self.span_loss_candidates == "all":
+            # naive mask
+            float_match_label_mask = match_label_mask.view(batch_size, -1).float()
+        else:
+            # use only pred or golden start/end to compute match loss
+            start_preds = start_logits > 0
+            end_preds = end_logits > 0
+            if self.span_loss_candidates == "gold":
+                match_candidates = ((start_labels.unsqueeze(-1).expand(-1, -1, seq_len) > 0)
+                                    & (end_labels.unsqueeze(-2).expand(-1, seq_len, -1) > 0))
+            else:
+                match_candidates = torch.logical_or(
+                    (start_preds.unsqueeze(-1).expand(-1, -1, seq_len)
+                     & end_preds.unsqueeze(-2).expand(-1, seq_len, -1)),
+                    (start_labels.unsqueeze(-1).expand(-1, -1, seq_len)
+                     & end_labels.unsqueeze(-2).expand(-1, seq_len, -1))
+                )
+            match_label_mask = match_label_mask & match_candidates
+            float_match_label_mask = match_label_mask.view(batch_size, -1).float()
+        if self.loss_type == "bce":
+            start_loss = self.bce_loss(start_logits.view(-1), start_labels.view(-1).float())
+            start_loss = (start_loss * start_float_label_mask).sum() / start_float_label_mask.sum()
+            end_loss = self.bce_loss(end_logits.view(-1), end_labels.view(-1).float())
+            end_loss = (end_loss * end_float_label_mask).sum() / end_float_label_mask.sum()
+            match_loss = self.bce_loss(span_logits.view(batch_size, -1), match_labels.view(batch_size, -1).float())
+            match_loss = match_loss * float_match_label_mask
+            match_loss = match_loss.sum() / (float_match_label_mask.sum() + 1e-10)
+        else:
+            start_loss = self.dice_loss(start_logits, start_labels.float(), start_float_label_mask)
+            end_loss = self.dice_loss(end_logits, end_labels.float(), end_float_label_mask)
+            match_loss = self.dice_loss(span_logits, match_labels.float(), float_match_label_mask)
+
+        return self._combine_loss(start_loss, end_loss, match_loss)
+
+    def _combine_loss(self, start_loss, end_loss, match_loss):
+        return (
+            torch.exp(-self.log_vars[0]) * start_loss + self.log_vars[0]
+            + torch.exp(-self.log_vars[1]) * end_loss + self.log_vars[1]
+            + torch.exp(-self.log_vars[2]) * match_loss + self.log_vars[2]
+        )
